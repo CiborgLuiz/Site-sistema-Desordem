f189dd2e48eddcbc96c2b023cddc149b92818bba
CiborgLuiz <jamillajaninenutri@gmail.com>
2026-05-30 17:40:04 -0300

    Correção de caminho do banco de dados

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,13 @@
 "use strict";
 
-module.exports = require("../server");
+const app = require("../server");
+
+module.exports = (req, res) => {
+  // Vercel pode repassar o caminho com ou sem o prefixo /api.
+  // Normalizamos para garantir que as rotas do Express sejam encontradas.
+  if (!req.url.startsWith("/api")) {
+    req.url = `/api${req.url}`;
+  }
+
+  return app(req, res);
+};
